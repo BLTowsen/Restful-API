2ea1125a80a8484df9c8ea651ba8b48953ca8fd4
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-30 23:41:56 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
 
 const app = express();
 
+app.use(bodyParser.json());
 //Import Routes
 const postsRoute = require('./routes/posts');
+const usersRoute = require('./routes/users');
 
 app.use('/posts', postsRoute);
+app.use('/users', usersRoute);
+
+
+//Home route
+app.get('/', (req, res) => {
+    res.send('We are on home');
+    res.end();
+});
 
 //Connect To DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
