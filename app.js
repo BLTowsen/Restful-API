d8c30aa66164d25d3a97cf210eec8a00521e7ed4
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-31 11:21:32 +0200

    Added routes for posting, patching, getting and deleting posts

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
 
+//Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 //Import Routes
 const postsRoute = require('./routes/posts');
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+//GET BACK ALL THE POSTS
 router.get('/', async (req, res) => {
     try{
         const posts = await Post.find();
@@ -16,6 +17,7 @@ router.get('/specific', (req, res) => {
     res.end();
 });
 
+//SUBMITS A POST
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -28,5 +30,36 @@ router.post('/', async (req, res) => {
         res.json({message: err});
     }
 });
+
+//SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//Delete Post
+router.delete('/:postId', async (req, res) => {
+    try{
+        const removedPost = await Post.deleteOne({_id: req.params.postId });
+        res.json(removedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//Update a post
+router.patch('/:postId', async (req, res) => {
+    try{
+        const updatedPost = await Post.updateOne({_id: req.params.postId}, {$set: {title: req.body.title}});
+        res.json(updatedPost);
+
+    }catch(err){
+        res.json({message: err});
+    }
+})
 
 module.exports = router;
