531b5bea76245122685ed282bcbeb732ed08da13
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-08-03 12:03:40 +0200

    Correct export and variable type

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = mongoose.Schema({
         required: true
     },
     age: {
-        type: Int16Array,
+        type: Number,
         required: false
     },
     address: {
@@ -15,4 +15,4 @@ const UserSchema = mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('Users', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
